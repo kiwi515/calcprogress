3c352e76852e892d5c0f8946b627ac37d58c9455
kiwi <49212064+kiwi515@users.noreply.github.com>
2022-01-27 13:28:40 -0500

    Add example slice group

--- main.py
+++ main.py
@@ -1,33 +1,30 @@
 from asm_section_list import AsmSectionList
 from dol import Dol
 from cw_map import Map
-from progress import Slice, SliceGroup, calc_generic_progress
+from progress import Slice, SliceGroup, calc_generic_progress, calc_slice_progress
 from obj_files_mk import ObjFilesMk
 
 """Configure these paths for your project"""
-# DOL_PATH = "build/main.dol"
-# MAP_PATH = "build/ogws_us_r1.map"
-# ASM_PATH = "asm"
-
-DOL_PATH = "build/pik/baserom.dol"
-MAP_PATH = "build/pik/pikminold.map"
-ASM_PATH = "asm/"
-OBJ_FILES_PATH = "build/pik/obj_files.mk"
+DOL_PATH = "build/main.dol"
+MAP_PATH = "build/ogws_us_r1.map"
+ASM_PATH = "asm"
+OBJ_FILES_PATH = "build/obj_files.mk"
 ASM_FILE_EXT = ".s"
 
-# """Default slice group for all code in the DOL."""
-# NW4R_SLICES = [
-# ]
-# """
-# All DOL slice groups.
-# - This is designed for tracking multiple slices together.
-# - The script will always display generic code/data progress,
-#   but you can add groups here to track things like libraries.
-# (Some examples are below)
-# """
-# DOL_SLICE_GROUPS = [
-#     SliceGroup("NW4R", NW4R_SLICES),
-# ]
+"""Example slice to track NW4R code."""
+NW4R_SLICES = [
+    # NW4R .text section
+    Slice(0x800076e0, 0x800838a8)
+]
+"""All DOL slice groups.
+- This is designed for tracking multiple slices together.
+- The script will always display generic code/data progress,
+  but you can add groups here to track things like libraries.
+(Some examples are below)
+"""
+DOL_SLICE_GROUPS = [
+    SliceGroup("NW4R", NW4R_SLICES),
+]
 
 def main():
     # Open DOL
@@ -35,7 +32,7 @@ def main():
 
     # Open link map
     # Set 'old_linker' if you use the old map format
-    dol_map = Map(MAP_PATH, old_linker=True)
+    dol_map = Map(MAP_PATH, old_linker=False)
 
     # Compile list of asm files
     obj_files = ObjFilesMk(OBJ_FILES_PATH, ASM_PATH, ASM_FILE_EXT)
@@ -45,5 +42,9 @@ def main():
 
     # Calculate generic progress (code/data)
     calc_generic_progress(dol, asm_list.sections)
+    # Calculate progress of slices
+    print("Slices:")
+    for group in DOL_SLICE_GROUPS:
+        calc_slice_progress(group, asm_list.sections)
 
 main()
--- progress.py
+++ progress.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+from pickle import FALSE
 from dol import Dol
 from asm_section_list import AsmSection, AsmSectionType
 
@@ -7,10 +8,29 @@ class Slice:
     start: int
     end: int
 
+    def size(self) -> int:
+        assert self.end > self.start
+        return self.end - self.start
+
+    def contains_section(self, sect: AsmSection) -> bool:
+        return self.start <= sect.start and self.end > sect.start + sect.size
+
 @dataclass
 class SliceGroup:
     name: str
     slices: list[Slice]
+
+    def total_size(self) -> int:
+        size = 0
+        for _slice in self.slices:
+            size += _slice.size()
+        return size
+
+    def contains_section(self, sect: AsmSection) -> bool:
+        for _slice in self.slices:
+            if _slice.contains_section(sect):
+                return True
+        return False
 
 def calc_generic_progress(dol: Dol, asm_list: list[AsmSection]):
     # Sum up code/data in ASM
@@ -33,5 +53,24 @@ def calc_generic_progress(dol: Dol, asm_list: list[AsmSection]):
     # Percentages
     code_percent = decomp_code_size / dol_code_size
     data_percent = decomp_data_size / dol_data_size
-    print(f"\tCode sections: {decomp_code_size} / {dol_code_size} bytes in src ({code_percent:%})")
-    print(f"\tData sections: {decomp_data_size} / {dol_data_size} bytes in src ({data_percent:%})")
+    print(f"Code sections: {decomp_code_size} / {dol_code_size} bytes in src ({code_percent:%})")
+    print(f"Data sections: {decomp_data_size} / {dol_data_size} bytes in src ({data_percent:%})")
+
+def calc_slice_progress(slices: SliceGroup, asm_list: list[AsmSection]):
+    asm_slice_size = 0
+    for section in asm_list:
+        if slices.contains_section(section):
+            if section.type == AsmSectionType.CODE:
+                asm_slice_size += section.size
+            elif section.type == AsmSectionType.DATA:
+                asm_slice_size += section.size
+            else:
+                assert False, f"Invalid section type ({section.type})!"
+
+    # Dol sizes
+    dol_slice_size = slices.total_size()
+    # Decompiled sizes
+    decomp_slice_size = dol_slice_size - asm_slice_size
+    # Percentages
+    slice_percent = decomp_slice_size / dol_slice_size
+    print(f"\t{slices.name}: {decomp_slice_size} / {dol_slice_size} bytes in src ({slice_percent:%})")
